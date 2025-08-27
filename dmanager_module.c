7331dfb8fb0809855b5471140064161989a33e40
SedovAV <72596005+SedovAV@users.noreply.github.com>
2025-08-27 22:15:29 +0500

    Add files via upload

---
+++ dmanager_module.c
@@ -0,0 +1,50 @@
+// src/dmanager_module.c
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#include "door_struct.h"
+
+#define DOORS_COUNT 15
+
+void initialize_doors(struct door* doors);
+
+int main()
+{
+    struct door doors[DOORS_COUNT];
+
+    initialize_doors(doors);
+
+    for (int i = 0; i < DOORS_COUNT; i++) {
+        doors[i].status = 0;
+    }
+
+    for (int i = 0; i < DOORS_COUNT - 1; i++) {
+        for (int j = 0; j < DOORS_COUNT - i - 1; j++) {
+            if (doors[j].id > doors[j + 1].id) {
+                struct door temp = doors[j];
+                doors[j] = doors[j + 1];
+                doors[j + 1] = temp;
+            }
+        }
+    }
+
+    for (int i = 0; i < DOORS_COUNT; i++) {
+        printf("%d, %d\n", doors[i].id, doors[i].status);
+    }
+
+    return 0;
+}
+
+void initialize_doors(struct door* doors)
+{
+    srand(time(0));
+
+    int seed = rand() % MAX_ID_SEED;
+    for (int i = 0; i < DOORS_COUNT; i++)
+    {
+        doors[i].id = (i + seed) % DOORS_COUNT;
+        doors[i].status = rand() % 2;
+    }
+}
